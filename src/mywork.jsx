a5d2a0aedd32106afecbc40569eb09fef2c21851
devanshwebhost <devanshrajput032006@gmail.com>
2025-02-22 20:06:44 +0530

    mywork.jsx

--- src/mywork.jsx
+++ src/mywork.jsx
@@ -19,9 +19,9 @@ const Workdata = [
     {
       "id": "project-3-para",
       "title": "FME",
-      "description": "This is a Tic Tac Toe game I made with only HTML, CSS and JAVASCRIPT. This is a fully functional game you can play with others. I used many functions to make this game. I made this game to check my skills. I actually learnt many things and functions in js. I think it's a good practice of js. After making this project, my logical thinking has now enhanced. Click on the image to see the full webpage.",
+      "description": "FME is a media agency this is only a prototype page of the website this project is in under development, only made with javascript functionality and html and usind Bootstrap for styles",
       "imageBackground": "img-background-3",
-      "imageLink": "https://devanshrajputgames.netlify.app/tic%20tac%20toe.html",
+      "imageLink": "https://fusionmediaelite.netlify.app",
       "imageId": "card-3-img"
     },
     {
